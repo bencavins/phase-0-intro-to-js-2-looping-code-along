6832bf456316a621a0fe47a9a72e4d56905f2730
Ben Cavins <ben.cavins@flatironschool.com>
2022-12-08 15:11:06 -0500

    commit tree 97395262536fd69b5fde575659594991fcb69a55

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    const result = [];
+    for (let i = 0; i < names.length; i++) {
+        result.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return result;
+}
+
+function countDown(n) {
+    while (n >= 0) {
+        console.log(n);
+        n -= 1;
+    }
+}
